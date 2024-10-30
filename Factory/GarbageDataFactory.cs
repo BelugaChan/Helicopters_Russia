7292f09b7e2e73cfcab7549271a437b06daf97b3
BelugaChan <tuchnyy@list.ru>
2024-10-30 16:43:40 +0300

    Minor fixes

--- Factory/GarbageDataFactory.cs
+++ Factory/GarbageDataFactory.cs
@@ -1,16 +1,17 @@
 ﻿using MinHash.Interfaces;
 using MinHash.Models;
+using NPOI.SS.UserModel;
 using OfficeOpenXml;
 
 namespace MinHash.Factory
 {
     public class GarbageDataFactory : IEntityFactory<GarbageData>
     {
-        public GarbageData CreateFromRow(ExcelWorksheet worksheet, int row)
+        public GarbageData CreateFromRow(IRow row)
         {
             return new GarbageData()
             {
-                ShortName = worksheet.Cells[row,2].Value?.ToString(),
+                ShortName = row.GetCell(1)?.ToString()
             };
         }
     }
